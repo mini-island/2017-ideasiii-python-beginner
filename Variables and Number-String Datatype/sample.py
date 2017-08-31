d11fee8f32ccbe7e0e6951fa3ba2c2c04ad9aa02
Yung-Chung Ku <ycku@csie.org>
2017-08-31 19:21:20 +0800

    Update sample.py

--- Variables and Number-String Datatype/sample.py
+++ Variables and Number-String Datatype/sample.py
@@ -1,6 +1,14 @@
 # Run this: https://repl.it/K701/3
 # '#'是註解
 
-var = 'Hello World'
+# 自動型別
+var = 'Hello World'  # string
 print(var)
+var = 100            # int
+print(var+10)
+
+# 沒有 overflow
+var = 17**3000  # 17的3000次方
+print(var)
+
 
